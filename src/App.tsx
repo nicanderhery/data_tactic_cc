7047940173731d46f8983df4b52019eb97b0eabd
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-12-14 10:51:03 +0100

    Add @mantine/dates and @mantine/modals packages
    Add due date features, modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import { Card, Checkbox, Flex, List, Text, TextInput } from '@mantine/core';
+import {
+  ActionIcon,
+  Box,
+  Button,
+  Card,
+  Checkbox,
+  Flex,
+  List,
+  Modal,
+  ScrollArea,
+  Text,
+  TextInput,
+  rem,
+} from '@mantine/core';
+import { DateInput, TimeInput } from '@mantine/dates';
+import { useDisclosure } from '@mantine/hooks';
+import { IconClock } from '@tabler/icons-react';
 import React from 'react';
 import './App.css';
 
 interface Todo {
   id: string;
   text: string;
+  dueDate?: Date;
 }
 
 function App() {
+  const [opened, { open, close }] = useDisclosure(false);
+  const [todoString, setTodoString] = React.useState<string>('');
+  const [dueDate, setDueDate] = React.useState<Date | null>(null);
+  const [dueTime, setDueTime] = React.useState<string>('');
+  const clockRef = React.useRef<HTMLInputElement>(null);
+
+  const pickerControl = (
+    <ActionIcon variant="subtle" color="gray" onClick={() => clockRef.current?.showPicker()}>
+      <IconClock style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+    </ActionIcon>
+  );
+
   const [todos, setTodos] = React.useState<Todo[]>([]);
   const [completed, setCompleted] = React.useState<Todo[]>([]);
 
@@ -15,30 +44,38 @@ function App() {
   React.useEffect(() => {
     const todos = localStorage.getItem('todos');
     if (todos) {
-      console.log('Loaded from local storage for todos');
-      setTodos(JSON.parse(todos) as Todo[]);
+      const parsed = JSON.parse(todos) as Todo[];
+      parsed.forEach((todo) => {
+        todo.dueDate = todo.dueDate ? new Date(todo.dueDate) : undefined;
+      });
+      setTodos(parsed);
     }
 
     const completed = localStorage.getItem('completed');
     if (completed) {
-      console.log('Loaded from local storage for completed');
       setCompleted(JSON.parse(completed) as Todo[]);
     }
   }, []);
 
   // Save to local storage
   React.useEffect(() => {
-    if (todos.length > 0) {
+    if (todos.length > 0 || completed.length > 0) {
       localStorage.setItem('todos', JSON.stringify(todos));
-    }
-    if (completed.length > 0) {
       localStorage.setItem('completed', JSON.stringify(completed));
     }
   }, [todos, completed]);
 
+  // Clear due date when modal is closed
+  React.useEffect(() => {
+    if (!opened) {
+      setDueDate(null);
+      setDueTime('');
+    }
+  }, [opened]);
+
   return (
     <Flex p="lg" justify="center" gap="lg">
-      <Card shadow="sm" padding="lg" radius="md" mih="40rem" miw="30rem">
+      <Card shadow="sm" padding="lg" radius="md" mih="40rem" w="30rem">
         <Flex h="100%" direction="column" justify="space-between">
           <Flex h="100%" direction="column" justify="flex-start">
             <Text fw={700} size="lg">
@@ -54,44 +91,58 @@ function App() {
               </Text>
             )}
             <List pt="md">
-              {todos.map((todo) => {
-                return (
-                  <List.Item
-                    key={todo.id}
-                    icon={
-                      <Checkbox
-                        onChange={(event) => {
-                          if (event.currentTarget.checked) {
-                            setCompleted((prev) => [...prev, todo]);
-                            setTodos((prev) => prev.filter((item) => item.id !== todo.id));
-                          }
-                        }}
-                      />
-                    }
-                  >
-                    <Text>{todo.text}</Text>
-                  </List.Item>
-                );
-              })}
+              <ScrollArea h="40rem" scrollbars="y">
+                {todos.map((todo) => {
+                  return (
+                    <List.Item
+                      key={todo.id}
+                      icon={
+                        <Checkbox
+                          onChange={(event) => {
+                            if (event.currentTarget.checked) {
+                              setCompleted((prev) => [...prev, todo]);
+                              setTodos((prev) => prev.filter((item) => item.id !== todo.id));
+                            }
+                          }}
+                        />
+                      }
+                    >
+                      <Box w="25rem">
+                        <Text truncate="end">{todo.text}</Text>
+                      </Box>
+                      {todo.dueDate && (
+                        <Text c={todo.dueDate < new Date() ? 'red' : 'gray'} size="xs">
+                          due on {todo.dueDate.toLocaleString()}
+                        </Text>
+                      )}
+                    </List.Item>
+                  );
+                })}
+              </ScrollArea>
             </List>
           </Flex>
           <TextInput
             pt="md"
             placeholder="Add your new task here (Press Enter to add)"
+            value={todoString}
+            onChange={(event) => setTodoString(event.currentTarget.value)}
             onKeyDown={(event) => {
               if (event.key === 'Enter') {
                 const todo = {
                   id: new Date().getTime().toString() + Math.random().toString(36).substring(7),
-                  text: event.currentTarget.value,
+                  text: todoString,
                 };
                 setTodos((prev) => [...prev, todo]);
-                event.currentTarget.value = '';
+                setTodoString('');
               }
             }}
           />
+          <Button variant="light" disabled={!todoString} onClick={open} mt="md">
+            <Text size="sm">Add due date</Text>
+          </Button>
         </Flex>
       </Card>
-      <Card shadow="sm" padding="lg" radius="md" mih="40rem" miw="30rem">
+      <Card shadow="sm" padding="lg" radius="md" mih="40rem" w="30rem">
         <Flex h="100%" direction="column" justify="space-between">
           <Flex h="100%" direction="column" justify="flex-start">
             <Text fw={700} size="lg">
@@ -107,30 +158,77 @@ function App() {
               </Text>
             )}
             <List pt="md">
-              {completed.map((todo) => {
-                return (
-                  <List.Item
-                    key={todo.id}
-                    icon={
-                      <Checkbox
-                        defaultChecked
-                        onChange={(event) => {
-                          if (!event.currentTarget.checked) {
-                            setTodos((prev) => [...prev, todo]);
-                            setCompleted((prev) => prev.filter((item) => item.id !== todo.id));
-                          }
-                        }}
-                      />
-                    }
-                  >
-                    <Text td="line-through">{todo.text}</Text>
-                  </List.Item>
-                );
-              })}
+              <ScrollArea h="40rem" scrollbars="y">
+                {completed.map((todo) => {
+                  return (
+                    <List.Item
+                      key={todo.id}
+                      icon={
+                        <Checkbox
+                          defaultChecked
+                          onChange={(event) => {
+                            if (!event.currentTarget.checked) {
+                              setTodos((prev) => [...prev, todo]);
+                              setCompleted((prev) => prev.filter((item) => item.id !== todo.id));
+                            }
+                          }}
+                        />
+                      }
+                    >
+                      <Box w="25rem">
+                        <Text truncate="end" td="line-through">
+                          {todo.text}
+                        </Text>
+                      </Box>
+                    </List.Item>
+                  );
+                })}
+              </ScrollArea>
             </List>
           </Flex>
         </Flex>
       </Card>
+
+      <Modal opened={opened} onClose={close} size="md">
+        <Flex direction="column" gap={'md'}>
+          <Modal.Title>Choose date</Modal.Title>
+          <DateInput
+            minDate={new Date()}
+            value={dueDate}
+            onChange={setDueDate}
+            label="Due date"
+            placeholder="Due date"
+          />
+          <TimeInput
+            label="Due time"
+            ref={clockRef}
+            rightSection={pickerControl}
+            value={dueTime}
+            onChange={(event) => setDueTime(event.currentTarget.value)}
+          />
+          <Button
+            variant="light"
+            onClick={() => {
+              // Check if due date is empty
+              if (!dueDate) {
+                alert('Please choose a due date');
+                return;
+              }
+
+              const todo = {
+                id: new Date().getTime().toString() + Math.random().toString(36).substring(7),
+                text: todoString,
+                dueDate: new Date(`${dueDate?.toDateString()} ${dueTime}`),
+              };
+              setTodos((prev) => [...prev, todo]);
+              setTodoString('');
+              close();
+            }}
+          >
+            <Text size="sm">Add todo</Text>
+          </Button>
+        </Flex>
+      </Modal>
     </Flex>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 // Core styles are required for all packages
 import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
